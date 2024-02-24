03cda48472a77e3624215e1bb2b420b05bc1231c
a0311759 <142903764+a0311759@users.noreply.github.com>
2024-02-24 14:51:09 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,11 +6,16 @@ app = Flask(__name__)
 # Replace 'YOUR_API_KEY' with your actual Google API key
 API_KEY = 'Api_keys'
 
-# Define the topics or keywords you are interested in
-TOPICS = ['programming', 'Engineering', 'personal finance', 'business']
+TOPICS = ['programming','math','soft skills','science', 'engineering', 'personal finance', 'business']
 
 # Number of videos to fetch for each topic
-NUM_VIDEOS = 5
+NUM_VIDEOS = 16
+
+# Keywords to exclude from the search results
+EXCLUDE_KEYWORDS = ['youtube short', 'shorts', 'reels', 'meme', 'vlog','rost']
+
+def is_video_excluded(title):
+    return any(keyword in title.lower() for keyword in EXCLUDE_KEYWORDS)
 
 def get_youtube_data(api_key, topics, num_videos):
     youtube = build('youtube', 'v3', developerKey=api_key)
@@ -30,8 +35,11 @@ def get_youtube_data(api_key, topics, num_videos):
         for item in response['items']:
             video_id = item['id']['videoId']
             video_title = item['snippet']['title']
-            thumbnail_url = item['snippet']['thumbnails']['medium']['url']
-            videos.append({'id': video_id, 'title': video_title, 'thumbnail': thumbnail_url, 'topic': topic})
+
+            # Check if the video title contains excluded keywords
+            if not is_video_excluded(video_title):
+                thumbnail_url = item['snippet']['thumbnails']['medium']['url']
+                videos.append({'id': video_id, 'title': video_title, 'thumbnail': thumbnail_url, 'topic': topic})
 
     return videos
 
@@ -48,11 +56,16 @@ def index():
 
     return render_template('index.html', videos=videos, search_query=search_query)
 
-@app.route('/watch/<video_id>')
-def watch(video_id):
-    # Redirect to the YouTube video page
-    youtube_url = f'https://www.youtube.com/watch?v={video_id}'
-    return redirect(youtube_url)
+# Add a new route to handle search redirects
+@app.route('/search')
+def search_redirect():
+    search_query = request.args.get('q', '')
+    if search_query:
+        # Redirect to the homepage with the search query appended to the URL
+        return redirect(url_for('index', q=search_query))
+    else:
+        # If no search query is provided, redirect to the homepage
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
